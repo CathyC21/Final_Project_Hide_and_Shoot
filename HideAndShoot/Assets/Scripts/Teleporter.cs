4283359be198f4262ae04d73ad2c139e9f571d88
CathyC21 <cc4406@nyu.edu>
2015-11-21 22:17:40 -0500

    jordan's environment scene

---
+++ HideAndShoot/Assets/Scripts/Teleporter.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour {
+    public Transform teleOut1;
+    public Transform teleOut2;
+    //public Transform previous;
+    public bool teleport = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter (Collider other){
+        if (other.GetComponent<Rigidbody>())
+        {
+          
+            int randomNum = (int)Random.Range(1, 2);
+            if (randomNum == 1 && teleport == false)
+            {
+               
+                other.transform.position = teleOut1.transform.position;
+                
+                
+            }
+
+            if (randomNum == 2 && teleport == false)
+            {
+               
+                other.transform.position = teleOut1.transform.position;
+               
+
+            }
+        }
+
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Rigidbody>())
+        {
+            teleport = true;
+        }
+    }
+
+   
+}
