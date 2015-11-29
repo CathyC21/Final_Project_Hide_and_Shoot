624ed99776b4733014f51ebc5b6d383d3534c074
CathyC21 <cc4406@nyu.edu>
2015-11-28 21:08:45 -0500

    sfx for gun and death

---
+++ Final Project - sfx/Assets/Scripts/Teleporter.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour {
+    public Transform teleOut1;
+    public Transform teleOut2;
+    //public Transform previous;
+    public bool teleport = false;
+
+//	[Header("Music")]
+//	public AudioSource sfx;
+//	public AudioClip sfx_tele;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter (Collider other){
+        if (other.GetComponent<Rigidbody>())
+        {
+          
+            int randomNum = (int)Random.Range(1, 2);
+            if (randomNum == 1 && teleport == false)
+            {
+               
+                other.transform.position = teleOut1.transform.position;
+                
+                
+            }
+
+            if (randomNum == 2 && teleport == false)
+            {
+               
+                other.transform.position = teleOut1.transform.position;
+               
+
+            }
+        }
+
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Rigidbody>())
+        {
+            teleport = true;
+			//sound 
+//			sfx.clip = sfx_tele;
+//			sfx.Play();
+        }
+    }
+
+   
+}
